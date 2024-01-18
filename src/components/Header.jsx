1c5c27217d6b0240369db4a6e7d730d9912d6263
Kimduck9 <156270789+Kimduck9@users.noreply.github.com>
2024-01-18 09:51:15 +0900

    6_header 완성

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 const headerNAV = [
     {
         title:'intro',
@@ -23,13 +23,19 @@ const headerNAV = [
 ]
 
 const Header = () => {
+    const [show,setShow] = useState(false);
+    const toggleMenu =()=>{
+        setShow((prewShow)=>!prewShow);
+    }
+
+
     return (
       <header id='header'>
         <div className="header_inner">
             <div className="header_logo">
-                <a href="#">Portfolio <em>react</em></a>
+                <a href="/">Portfolio <em>react</em></a>
             </div>
-            <nav className='header_nav' aria-label='메인메뉴'>
+            <nav className={`header_nav ${show ? "show":""}`} aria-label='메인메뉴'>
                 <ul>
                     {headerNAV.map((nav,key)=>(
                         <li key={key}>
@@ -38,7 +44,7 @@ const Header = () => {
                     ))}
                 </ul>
             </nav>
-            <div className="header_mobile" id='header_toggle' aria-aria-controls='primary-menu' aria-expanded='false' role='button' tabIndex='0'>
+            <div className="header_mobile" id='header_toggle' aria-aria-controls='primary-menu' aria-expanded={show ? "true":"false"} role='button' tabIndex='0' onClick={toggleMenu}>
                 <span></span>
             </div>
         </div>
