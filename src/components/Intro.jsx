9448fb3a5580a29088df04af4a0a75842f10aced
Kimduck9 <156270789+Kimduck9@users.noreply.github.com>
2024-01-18 10:48:52 +0900

    7_intro 완성

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
+import about from '../assets/img/about.jpg'
+
+const introText={
+  tittle:'port developer',
+  desc:["talent is","found at the end of the","effort"]
+}
 
 const Intro = () => {
   return (
-    <div>
-      Intro
-    </div>
+    <section id="intro">
+      <div className="intro_inner">
+        <h1 className='intro_tit'>{introText.tittle}</h1>
+        <div className="intro_lines" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+        <div className="intro_txt">
+          <div className="text">
+            <div>{introText.desc[0]}</div>
+            <div>{introText.desc[1]}</div>
+            <div>{introText.desc[2]}</div>
+          </div>
+          <div className="img">
+            <img src={about} alt="코트 입은 수" />
+          </div>
+        </div>
+        <div className="intro_lines bottom" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+      </div>
+    </section>
   )
 }
 
